ced4fccb4de6fa1bcd774af01379494756689043
Ess <pooster@gmail.com>
2018-06-13 18:47:44 -0400

    Undid the unnecessary basher changes

--- pkg/basher/basher.go
+++ pkg/basher/basher.go
@@ -5,22 +5,16 @@ import (
 	"syscall"
 )
 
-var Run func(command string) int
+var Run = func(command string) int {
+	cmd := exec.Command("bash", "-c", command)
+	var waitStatus syscall.WaitStatus
 
-func init() {
-	if Run == nil {
-		Run = func(command string) int {
-			cmd := exec.Command("bash", "-c", command)
-			var waitStatus syscall.WaitStatus
-
-			if err := cmd.Run(); err != nil {
-				if exitError, ok := err.(*exec.ExitError); ok {
-					waitStatus = exitError.Sys().(syscall.WaitStatus)
-					return waitStatus.ExitStatus()
-				}
-			}
-
-			return 0
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			return waitStatus.ExitStatus()
 		}
 	}
+
+	return 0
 }
